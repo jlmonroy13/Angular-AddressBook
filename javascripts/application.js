f6c81f204cce2fcada9469104e4d12928cbcde04
jlmonroy13 <jlmonroy13@gmail.com>
2016-04-27 16:27:03 -0500

    modal contacts

--- javascripts/application.js
+++ javascripts/application.js
@@ -4,16 +4,24 @@
   angular
     .module('apiKoombeaProfiles')
     .controller('KoombeaProfilesCtrl', KoombeaProfilesCtrl);
-
     KoombeaProfilesCtrl.$inject = ['$http'];
-    
     function KoombeaProfilesCtrl($http){
       var store = this;
       store.contacts = [];
+      store.modal = false;
       $http.get('http://localhost:3000/profiles?token=1234').success(function(data) {
         store.contacts = data;
         console.log(store.contacts);
       });
+      store.getProfile = function(data) {
+        store.actualProfile = data;
+        store.openModal();
+      };
+      store.openModal = function() {
+        store.modal = true;
+      };
+      store.closeModal = function() {
+        store.modal = false;
+      };
     }; 
-
 })();
